0b93b22cde7f8ce6219407d8082877c4cc24d82a
Jason M. Chalmers <jason.m.chalmers@gmail.com>
2018-08-03 11:20:47 -0400

    Added scrubbing to support AND(&), NOT(!), OR(|), and operator precedence ()

--- aws/lambda/get_lore.py
+++ aws/lambda/get_lore.py
@@ -2,16 +2,66 @@ import os, pg8000, re, string
 
 
 def build_search_string(raw_string):
-
     # remove leading/trailing whitespace
     raw_string = raw_string.strip()
     # remove any punctuation
-    raw_string = raw_string.translate(raw_string.maketrans('', '', string.punctuation))
-    # replace spaces with ampersands
+    #raw_string = raw_string.translate(raw_string.maketrans('', '', string.punctuation))
+    raw_string = raw_string.translate(raw_string.maketrans('', '', '"#$%\'*+,-./:;<=>?@[\]^_`{}~'))
+    # Remove )(
+    bad_parens = re.compile('\)\s*\(')
+    raw_string = re.sub(bad_parens, ' ', raw_string)
+    # remove redundant spaces
     raw_string = re.sub('\s\s+', ' ', raw_string)
-    raw_string = raw_string.replace(' ', ' & ')
 
-    # print(raw_string)
+    # replace keywords 'and', 'not', and 'or' with '&', '!' and '|'
+    keywords = [
+        {
+            'old': 'and',
+            'new': '&'
+        },
+        {
+            'old': 'not',
+            'new': '!'
+        },
+        {
+            'old': 'or',
+            'new': '|'
+        }
+    ]
+    print(raw_string)
+    for keyword in keywords:
+        keyword_sub = re.compile('\s{0}\s'.format(keyword['old']))
+
+        # must maintain whitespace for successive searches within the loop
+        raw_string = re.sub(keyword_sub, ' {0} '.format(keyword['new']), raw_string)
+        print(raw_string)
+
+    # replace single space between words with &
+    single_space = re.compile('([a-zA-Z0-9])\s([a-zA-Z0-9])')
+    raw_string = re.sub(single_space, '\\1&\\2', raw_string)
+    allowed_punc = ['(', ')', '&', '!', '|']
+
+    for punc in allowed_punc:
+        punc_sub = re.compile('\s*\{0}\s*'.format(punc))
+
+        raw_string = re.sub(punc_sub, punc, raw_string)
+
+    #raw_string = raw_string.replace(' and ', '&').replace(' or ', '|').replace(' & ', '&').replace(' | ', '|')
+
+    # Look for ! not preceded by & or | and replace with &!
+    invalid_not = re.compile('([a-zA-Z0-9])!')
+    raw_string = re.sub(invalid_not, '\\1&!', raw_string)
+
+    # Look for mismatched ( and )
+    open_parens = raw_string.count('(')
+    close_parens = raw_string.count(')')
+
+    if open_parens != close_parens:
+        print('{0} - {1}'.format(open_parens, close_parens))
+        # strip out all parentheses instead of trying to figure out matching
+        raw_string = raw_string.replace('(', '').replace(')', '')
+
+    print(raw_string)
     new_string = raw_string
 
     return new_string
@@ -59,3 +109,5 @@ def handler(event, context):
 
     #print(lore)
     return lore
+
+    #return lore_search
