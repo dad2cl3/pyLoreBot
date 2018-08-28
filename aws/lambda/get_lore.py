35d137a61b154745e690dba2dee6a1090a1ce1a2
Jason M. Chalmers <jason.m.chalmers@gmail.com>
2018-08-28 16:58:49 -0400

    Preliminary attempt to move scrub of grimoire card data in to the AWS Lambda function

--- aws/lambda/get_lore.py
+++ aws/lambda/get_lore.py
@@ -1,12 +1,23 @@
-import os, pg8000, re, string
+import json, os, pg8000, re, string
+
+
+with open ('grimoire_cleanup.json', 'r') as grimoire_cleanup_file:
+    cleanup_params = json.load(grimoire_cleanup_file)
+
+
+def scrub (raw_string):
+
+    for cleanup in cleanup_params['grimoire_cleanup']:
+        raw_string = raw_string.replace(cleanup['old'], cleanup['new'])
+
+    return raw_string
 
 
 def build_search_string(raw_string):
     # remove leading/trailing whitespace
     raw_string = raw_string.strip()
     # remove any punctuation
-    #raw_string = raw_string.translate(raw_string.maketrans('', '', string.punctuation))
-    raw_string = raw_string.translate(raw_string.maketrans('', '', '"#$%\'*+,-./:;<=>?@[\]^_`{}~'))
+    raw_string = raw_string.translate(raw_string.maketrans('', '', '"<>#$%\'*+,-./:;=?@[\]^_`{}~'))
     # Remove )(
     bad_parens = re.compile('\)\s*\(')
     raw_string = re.sub(bad_parens, ' ', raw_string)
@@ -28,27 +39,28 @@ def build_search_string(raw_string):
             'new': '|'
         }
     ]
-    print(raw_string)
+    #print(raw_string) # debugging
     for keyword in keywords:
         keyword_sub = re.compile('\s{0}\s'.format(keyword['old']))
 
         # must maintain whitespace for successive searches within the loop
         raw_string = re.sub(keyword_sub, ' {0} '.format(keyword['new']), raw_string)
-        print(raw_string)
+        #print(raw_string) # debugging
 
     # replace single space between words with &
     single_space = re.compile('([a-zA-Z0-9])\s([a-zA-Z0-9])')
     raw_string = re.sub(single_space, '\\1&\\2', raw_string)
-    print('Remove single space between words with & - {0}'.format(raw_string))
+    #raw_string = re.sub(single_space, '\\1<->\\2', raw_string)
+    #print('Remove single space between words with & - {0}'.format(raw_string)) # debugging
 
     # replace single space around operators with operator
-    allowed_punc = ['(', ')', '&', '!', '|']
+    allowed_punc = ['(', ')', '&', '!', '|', '<', '>']
 
     for punc in allowed_punc:
         punc_sub = re.compile('\s*\{0}\s*'.format(punc))
         raw_string = re.sub(punc_sub, punc, raw_string)
 
-    print('Replace single space around operators with operators - {0}'.format(raw_string))
+    #print('Replace single space around operators with operators - {0}'.format(raw_string)) # debugging
     #raw_string = raw_string.replace(' and ', '&').replace(' or ', '|').replace(' & ', '&').replace(' | ', '|')
 
     # Look for ! not preceded by & or | and replace with &!
@@ -58,11 +70,11 @@ def build_search_string(raw_string):
     # Look for combinations &| or |&
     bad_combo = re.compile('&\|([&\|!]{1,})')
     raw_string = re.sub(bad_combo, '&', raw_string)
-    print('Removing "&|" combination - {0}'.format(raw_string))
+    #print('Removing "&|" combination - {0}'.format(raw_string)) # debugging
 
     bad_combo = re.compile('\|&([&\|!]{1,})')
     raw_string = re.sub(bad_combo, '|', raw_string)
-    print('Removing "|&" combination - {0}'.format(raw_string))
+    #print('Removing "|&" combination - {0}'.format(raw_string)) # debugging
 
     # Look for redundant operators other than parentheses
     allowed_punc = ['&', '!', '\|']
@@ -76,11 +88,11 @@ def build_search_string(raw_string):
     close_parens = raw_string.count(')')
 
     if open_parens != close_parens:
-        print('{0} - {1}'.format(open_parens, close_parens))
+        #print('{0} - {1}'.format(open_parens, close_parens)) # debugging
         # strip out all parentheses instead of trying to figure out matching
         raw_string = raw_string.replace('(', '').replace(')', '')
 
-    print(raw_string)
+    #print(raw_string) # debugging
     new_string = raw_string
 
     return new_string
@@ -118,6 +130,25 @@ def handler(event, context):
 
         results = []
         for result in query_results:
+            #if result[0]['item_type'] == 'Grimoire':
+            item_name = scrub(result[0]['item_name'])
+            result[0]['item_name'] = item_name
+
+            item_description = result[0]['item_description']
+            if item_description != None:
+                item_description = scrub(item_description)
+                result[0]['item_description'] = item_description
+
+            '''lore_subtitle = result[0]['lore_subtitle']
+            if lore_subtitle != None:
+                lore_subtitle = scrub(result[0]['lore_subtitle'])
+                result[0]['lore_subtitle'] = lore_subtitle'''
+
+            #print('PREVIOUS: {0}'.format(result[0]['lore_description']))
+            lore_description = scrub(result[0]['lore_description'])
+            #print('FINAL: {0}'.format(lore_description))
+            result[0]['lore_description'] = lore_description
+
             results.append(result[0])
 
         if len(results) > 0:
